e71cc1b1997da89c9366c6f525a289d88a8796ff
lupeibei <lupeibei@bytedance.com>
2021-09-10 10:06:57 +0800

    新增部分代码

---
+++ src/JavaChar.java
@@ -0,0 +1,12 @@
+import sun.text.normalizer.UCharacter;
+
+/**
+ * @author lupeibei
+ * @date 2021/9/6 9:10 下午
+ */
+public class JavaChar {
+    public static void main(String[] args) {
+//        String i= "run";
+//        System.out.println(i.isLetter('r'));
+    }
+}
---
+++ src/num_math.java
@@ -0,0 +1,10 @@
+/**
+ * @author lupeibei
+ * @date 2021/9/5 11:51 下午
+ */
+public class num_math {
+    public static void main(String[]args){
+      System.out.println(Math.sin(30));
+
+    }
+}
